38577e47a8e746a5a9c1cf7a1430e1629cd290de
dilipcode <drsar8@gmail.com>
2016-04-27 14:23:28 +0530

    Finished CRUD operations on Book

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  before_action :fetch_book, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book= Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @book.update_attributes(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+
+  end
+
+  def show
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path, notice: "Book #{@book.title} deleted succesfully"
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title,:description)
+    end
+
+    def fetch_book
+      @book =Book.find(params[:id])
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env.development?
+  Book.find_or_create_by(title: "Emotional strength") do |book|
+    book.description= "Learn to stay strong in face of adversity"
+  end
+  Book.find_or_create_by(title: " Decision making") do |book|
+    book.description= "Taking decisions without fear or anger"
+  end
+  Book.find_or_create_by(title: " Influenced?") do |book|
+  book.description="Stop comparing youself with others and start measuring yourself"
+  end
+end
