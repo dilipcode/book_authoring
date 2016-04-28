c9fc98d7e0f61fb0b15ee3a104b3c6425557baf9
dilipcode <drsar8@gmail.com>
2016-04-28 08:15:46 +0530

    CRUD of sections completed

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,54 @@
 class SectionsController < ApplicationController
+  before_action :fetch_chapter
+  before_action :fetch_section, only: [:show, :edit, :update, :destroy]
   def index
   end
+
+  def new
+    @section = @chapter.sections.new
+  end
+
+  def create
+    @section = @chapter.sections.new(section_params)
+    if @section.save
+      redirect_to book_chapter_section_path(@book, @chapter, @section),notice: "Section created succesfully"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+  def edit
+
+  end
+  def update
+    if @section.update_attributes(section_params)
+      redirect_to book_chapter_section_path(@book,@chapter,@section), notice: "Section updated succesfully"
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @section.destroy
+    redirect_to book_chapter_path(@book, @chapter), notice: "Section deleted succesfully"
+  end
+
+
+  private
+    def fetch_chapter
+      @book = Book.find(params[:book_id])
+      @chapter = @book.chapters.find(params[:chapter_id])
+    end
+
+    def fetch_section
+      @section = @chapter.sections.find(params[:id])
+
+    end
+
+    def section_params
+      params.require(:section).permit(:title, :content)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427091018) do
+ActiveRecord::Schema.define(version: 20160428014902) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -29,5 +29,15 @@ ActiveRecord::Schema.define(version: 20160427091018) do
   end
 
   add_index "chapters", ["book_id"], name: "index_chapters_on_book_id"
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sections", ["chapter_id"], name: "index_sections_on_chapter_id"
 
 end
